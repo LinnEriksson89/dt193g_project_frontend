4e5ee13bea0763c04cd1dee6c25a70351e5597ab
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2026-05-06 16:22:12 +0200

    adding styling to register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,4 +1,6 @@
 <script>
+import PageHeader from '@/components/design/PageHeader.vue';
+
 export default {
     data() {
         return {
@@ -6,7 +8,8 @@ export default {
             email: "",
             password: "",
             password_confirmed: "",
-            errorMessage: ""
+            errorMessage: "",
+            userCreated: ""
         }
     },
     methods: {
@@ -25,6 +28,7 @@ export default {
                 this.name = "";
                 this.email = "";
                 this.password = "";
+                this.password_confirmed = "";
             } else {
                 this.errorMessage = "Lösenordet matchar inte.";
             }
@@ -42,44 +46,60 @@ export default {
 
                 if (!response.ok) {
                     this.errorMessage = "Användaren kunde inte skapas.";
+                } else {
+                    this.userCreated = "Användaren har lagts till."
                 }
             } catch (error) {
                 this.errorMessage = "Något gick fel när användaren skulle skapas.";
                 console.log(error);
             }
         }
+    },
+    components: {
+        PageHeader
     }
 }
 </script>
 
 <template>
-    <form v-on:submit.prevent="addUser">
-        <fieldset>
-            <legend>Skapa konto:</legend>
-            <span v-if="errorMessage">{{ errorMessage }}</span>
-            <br />
-            <label for="name">Användarnamn:</label>
-            <input type="text" v-model="name" required maxlength="32" minlength="4" size="25">
-            <span v-if="nameError">{{ nameError }}</span>
+    <PageHeader page-title="Registrera dig!" />
+    <p class="m-2 p-2 max-w-2xl">Använd formuläret nedan för att registrera ditt konto. Användarnamnet måste vara 4-25
+        tecken och lösenordet måste vara minst 12 tecken långt.</p>
+
+    <form v-on:submit.prevent="addUser" class="m-2 max-w-xl">
+        <fieldset class="border p-2 rounded-lg grid lg:grid-cols-3">
+
+            <legend class="font-bold p-2">Skapa konto:</legend>
+            <span v-if="errorMessage"
+                class="max-w-full border-4 border-red-600 text-lg font-bold p-2 m-2 text-center">{{ errorMessage }}</span>
             <br />
 
-            <label for="email">Email:</label>
-            <input type="email" v-model="email" name="email" id="email" required maxlength="128" size="25">
-            <span v-if="emailError">{{ emailError }}</span>
+            <label for="name" class="lg:col-start-1">Användarnamn:</label>
+            <input type="text" v-model="name" required maxlength="32" minlength="4" size="25"
+                class="border m-2 rounded-lg lg:col-start-2 lg:col-span-2 p-2">
             <br />
 
-            <label for="password">Lösenord:</label>
-            <input type="password" v-model="password" name="password" id="password" required minlength="12" size="40">
-            <label for="password_confirmed">Upprepa lösenord:</label>
+            <label for="email" class="lg:col-start-1">Email:</label>
+            <input type="email" v-model="email" name="email" id="email" required maxlength="128" size="25"
+                class="border m-2 rounded-lg lg:col-start-2 lg:col-span-2 p-2">
+            <br />
+
+            <label for="password" class="lg:col-start-1">Lösenord:</label>
+            <input type="password" v-model="password" name="password" id="password" required minlength="12" size="40"
+                class="border m-2 rounded-lg lg:col-start-2 lg:col-span-2 p-2">
+            <br />
+
+            <label for="password_confirmed" class="lg:col-start-1">Upprepa lösenord:</label>
             <input type="password" v-model="password_confirmed" name="password_confirmed" id="password_confirmed"
-                required minlength="12" size="40">
-            <span v-if="passwordError">{{ passwordError }}</span>
+                required minlength="12" size="40" class="border m-2 rounded-lg lg:col-start-2 lg:col-span-2 p-2">
             <br />
 
-            <div>
-                <input type="reset" value="Rensa">
-                <input type="submit" value="Skicka">
-                <span v-if="userCreated">{{ userCreated }}</span>
+            <div class="text-right lg:col-span-2">
+                <input type="reset" value="Rensa"
+                    class="border m-2 p-2 px-8 rounded-lg bg-red-950 text-neutral-300 border-neutral-300 hover:bg-neutral-300 hover:text-red-950 hover:border-red-950 focus:bg-neutral-300 focus:text-red-950 focus:border-red-950">
+                <input type="submit" value="Skicka"
+                    class="border m-2 p-2 px-8 rounded-lg bg-red-950 text-neutral-300 border-neutral-300 hover:bg-neutral-300 hover:text-red-950 hover:border-red-950 focus:bg-neutral-300 focus:text-red-950 focus:border-red-950">
+                <p v-if="userCreated">{{ userCreated }}</p>
             </div>
         </fieldset>
     </form>
